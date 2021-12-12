628e1de6387a9b3051dd8e2dcead31133370a88a
alexsteve647 <56225128+alexsteve647@users.noreply.github.com>
2021-12-12 20:58:27 +0200

    Update webserver.js

--- webserver.js
+++ webserver.js
@@ -1,1 +1,2 @@
-const express=require('express'),app=express();app.use(express.static('web'));app.listen(8080);
+const wssPort = process.env.PORT || 8080;
+const express=require('express'),app=express();app.use(express.static('web'));app.listen(wssPort);
